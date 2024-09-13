61de72f4ea48fa6d820fe1ec7c64cf139b91c456
Nguyễn Thị Quỳnh Mai <147372843+QuynhMai273na@users.noreply.github.com>
2024-09-13 13:36:35 +0700

    First project NodeJs

---
+++ express.js
@@ -0,0 +1,13 @@
+// Hello world voi Express
+const express = require('express');
+
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Welcome to my server!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
