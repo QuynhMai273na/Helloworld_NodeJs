491c09e5e90a26dddd7439a134e11ac520ffd2ba
Nguyễn Thị Quỳnh Mai <nguyenquynhmai273na@gmail.com>
2024-09-14 18:28:29 +0700

    Update sever.js

--- sever.js
+++ sever.js
@@ -9,6 +9,8 @@ const server = http.createServer((req, res)=>{
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
 });
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
